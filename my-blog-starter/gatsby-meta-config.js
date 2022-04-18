fec71834cffb081b52b68175d3cb94a2fe86fd08
jiyeongj <0708gyc@gmail.com>
2022-04-18 16:54:46 +0900

    수정

--- my-blog-starter/gatsby-meta-config.js
+++ my-blog-starter/gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Blog posted about ...`,
   author: `Ji Yeong Choe`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://jiyeongj.github.io/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
