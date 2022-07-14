66558505e59aca2102e138afb2bed311de878069
jiyeongj <0708gyc@gmail.com>
2022-07-14 15:41:09 +0900

    plugin-sitemap 관련 config 수정

--- my-blog-starter/gatsby-config.js
+++ my-blog-starter/gatsby-config.js
@@ -3,6 +3,7 @@ const metaConfig = require('./gatsby-meta-config')
 module.exports = {
   siteMetadata: metaConfig,
   plugins: [
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -96,8 +97,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://your-blog.netlify.app',
-        sitemap: 'https://your-blog.netlify.app/sitemap.xml',
+        host: 'https://jiyeongj.github.io/',
+        sitemap: 'https://jiyeongj.github.io/sitemap.xml',
         policy: [
           {
             userAgent: '*',
